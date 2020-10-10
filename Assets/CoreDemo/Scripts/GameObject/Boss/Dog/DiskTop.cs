cdd2320f96e223b9bfdc9086881a1ced636cf213
Vanot <2834552723@qq.com>
2020-10-11 00:11:01 +0800

    磁力

--- Assets/CoreDemo/Scripts/GameObject/Boss/Dog/DiskTop.cs
+++ Assets/CoreDemo/Scripts/GameObject/Boss/Dog/DiskTop.cs
@@ -6,17 +6,19 @@ using Core.Character;
 namespace Core.Dog{
     public class DiskTop : Creature{
         public Wed aimAt;
+        public bool active = false;
+        public float power = 50.0f;
 
         protected override void StateMachineInit(){
             stateMachine = new StateMachine();
             stateMachine.addState(new DiskTop_Normal(this));
             stateMachine.addState(new DiskTop_Active(this));
             stateMachine.addState(new DiskTop_Moving(this));
-            stateMachine.switchState("moving");
+            stateMachine.switchState("normal");
         }
         
         void Start(){
-
+            StateMachineInit();
 
         }
 
